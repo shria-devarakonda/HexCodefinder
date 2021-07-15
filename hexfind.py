fa0e8340845fe6343d002c305a8b0fb0e4835ca1
Shria Devarakonda <54900370+shria-devarakonda@users.noreply.github.com>
2021-07-15 19:29:13 +0530

    Update hexfind.py

--- hexfind.py
+++ hexfind.py
@@ -12,7 +12,7 @@ for i in range(int(input())):
         matchy = re.findall(regex_pattern, line)
         for i in matchy:
             print(i)
-            #matchyy.append(i[:-1])
+            matchyy.append(i)
     if "{" in line:
         #print("enter")
         a+=1
@@ -20,3 +20,7 @@ for i in range(int(input())):
         pass
 
 #print(matchyy)
+for i in matchyy:
+    with open("hexes.txt", "+a") as f:
+        f.write(i)
+        f.write("\n")
